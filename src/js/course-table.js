e368c065a634edc857b1457f2cc03750b7da5b0c
Yao <yao_yu.wu@futuregames.nu>
2024-02-12 13:28:12 +0100

    Sorting function
    - modification ecma-script html file
    - added sorting function in course-table.js file

--- src/js/course-table.js
+++ src/js/course-table.js
@@ -1,6 +1,12 @@
 "use strict";
 
 const url = "https://dahlgren.miun.se/ramschema_ht23.php";
+let coursesCodeEl = document.getElementById("course-code");
+let coursesNameEl = document.getElementById("course-name");
+let coursesProgressionEl = document.getElementById("course-progression");
+coursesCodeEl.addEventListener("click", sortCourseByCode);
+coursesNameEl.addEventListener("click", sortCourseByName);
+coursesProgressionEl.addEventListener("click", sortProgression);
 
 window.onload = init;
 
@@ -17,17 +23,67 @@ async function init() {
     }
 }
     
-// function that displays data
+// function that displays courses
 
 function displayCourses(courses) {
     const coursesEl = document.getElementById("courses-list");
+    coursesEl.innerHTML = ""; 
 
     courses.forEach((course) => {
         coursesEl.innerHTML += `
             <tr>
                 <td>${course.code}</td>
                 <td>${course.coursename}</td>
+                <td>${course.progression}</td>
             </tr>
             `;
     });
+}
+
+// function that sorts courses by code
+
+async function sortCourseByCode() {
+    try {
+        const response = await fetch(url);
+        let courses = await response.json();
+
+        courses.sort((a, b) =>
+        (a.code > b.code) ? 1 : -1);
+
+        displayCourses(courses);
+    } catch {
+        document.getElementById("error").innerHTML = "<p>Något gick fel. Försök igen senare!</p>";
+    } 
+}
+
+// function that sorts courses by name
+
+async function sortCourseByName() {
+    try {
+        const response = await fetch(url);
+        let courses = await response.json();
+
+        courses.sort((a, b) =>
+        (a.coursename > b.coursename) ? 1 : -1);
+
+        displayCourses(courses);
+    } catch {
+        document.getElementById("error").innerHTML = "<p>Något gick fel. Försök igen senare!</p>";
+    } 
+}
+
+// function that sorts courses by progression
+
+async function sortProgression() {
+    try {
+        const response = await fetch(url);
+        let courses = await response.json();
+
+        courses.sort((a, b) =>
+        (a.progression > b.progression) ? 1 : -1);
+
+        displayCourses(courses);
+    } catch {
+        document.getElementById("error").innerHTML = "<p>Något gick fel. Försök igen senare!</p>";
+    } 
 }
